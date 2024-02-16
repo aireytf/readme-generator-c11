5683b3c6924c8b847b1451c5537c4d00b93489e8
Tom Airey <aireytf19@gmail.com>
2024-02-16 20:01:27 +0000

    add utils folder, npm inquirer, & initial index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require('path'); 
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: ,
+            message: ,
+        }
+    ])
+    .then();
+}
+
+// function call to initialize program
+init();
+
