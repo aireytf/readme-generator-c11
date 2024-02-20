33ce3eb157eea9a83099c3926830ce515bf0aad1
Tom Airey <aireytf19@gmail.com>
2024-02-20 18:15:17 +0000

    amend sample README name

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function init() {
     inquirer
     .prompt(questions)
     .then((answers) => {
-        writeToFile("SampleREADME.md", answers)
+        writeToFile("READMEsample.md", answers)
     })
 };
 
