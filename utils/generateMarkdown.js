dc73789d170933e80cb7bb8c8c981b42b010e8a8
Tom Airey <aireytf19@gmail.com>
2024-02-20 17:00:43 +0000

    add layout to generateMarkdown.js file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,40 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}
+    return `
+    
+    # ${data.title}
+
+    ${data.description}
+
+    ## Table of Contents
+
+    1. [Installation](#installation)
+    2. [Usage](#usage)
+    3. [License](#license)
+    4. [Contributing](#contributing)
+    5. [Test](#tests)
+    6. [Questions & Contact]
+
+    ## Installation
+    ${data.installation}
+
+    ## Usage
+    ${data.usage}
+
+    ## License 
+    ${data.license}
+
+    ## Contributing
+    ${data.contributing}
+
+    ## Tests
+    ${data.tests}
+
+    ## Questions
+
+    If you should have any questions, please do not hesitate to contact me at:
+    Email: ${data.email}
+    GitHub: ${data.GitHubUser}
   
   `;
   }
